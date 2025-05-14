2d1d03b92b68c25bc9fb13f565665de9e6d778cd
Dylan Luc <dylanlucyyc@gmail.com>
2025-05-14 14:34:41 -0600

    Implement the hero section for homepage

---
+++ src/components/HomeHero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import HeroText from "../assets/images/+15-skywalk.svg";
+import HeroImage from "../assets/images/hero-walking.svg";
+
+function HomeHero() {
+  return (
+    <div className="flex h-[85vh] flex-col items-center justify-center border-b-1">
+      <img src={HeroText} alt="Hero Text" />
+      <div className="relative w-full">
+        <img
+          className="z-10 m-auto relative"
+          src={HeroImage}
+          alt="Hero Image"
+        />
+        <span className="w-full border-b border-dashed absolute bottom-20 z-0"></span>
+      </div>
+    </div>
+  );
+}
+
+export default HomeHero;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import useAuth from "../hooks/useAuth";
 import { Link } from "react-router-dom";
-
+import HomeHero from "../components/HomeHero";
 function HomePage() {
-  const { user, isInitialized, isAuthenticated } = useAuth();
+  const { isInitialized } = useAuth();
 
   if (!isInitialized) {
     return (
@@ -14,30 +14,9 @@ function HomePage() {
   }
 
   return (
-    <div>
-      {isAuthenticated ? (
-        <div>
-          <h1>Welcome, {user?.name}</h1>
-          <div>
-            <p>Email: {user?.email}</p>
-            <p>Role: {user?.role}</p>
-          </div>
-          <div>
-            <Link to="/profile">View Profile</Link>
-            <Link to="/settings">Settings</Link>
-          </div>
-        </div>
-      ) : (
-        <div>
-          <h1>Welcome to Skywalk</h1>
-          <p>Please sign in to access your account</p>
-          <div>
-            <Link to="/signin">Sign In</Link>
-            <Link to="/signup">Sign Up</Link>
-          </div>
-        </div>
-      )}
-    </div>
+    <>
+      <HomeHero />
+    </>
   );
 }
 
