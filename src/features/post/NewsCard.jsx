c89285f1c020c40d9a4a7787684aed72d1778110
Dylan Luc <dylanlucyyc@gmail.com>
2025-05-18 21:54:37 -0600

    Update tags out out, it is a string now

--- src/features/post/NewsCard.jsx
+++ src/features/post/NewsCard.jsx
@@ -14,10 +14,11 @@ function NewsCard({ post }) {
           alt={post.title}
           className="flex-shrink-0 object-cover w-[100px] h-[100px]"
         />
+        {console.log(post)}
         <div className="flex-grow min-w-0">
           <div className="flex justify-between items-start">
             <span className="text-sm font-semibold uppercase text-gray-600 block mb-1">
-              {post.tags?.[0] || "News"}
+              {post.tags || "News"}
             </span>
             <FavoriteButton postId={post._id} size="sm" />
           </div>
