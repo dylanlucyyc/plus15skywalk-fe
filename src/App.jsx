44e4b19eb7981e961615fa27c9f1d4d0ddc7bc2d
Dylan Luc <dylanlucyyc@gmail.com>
2025-05-18 21:39:15 -0600

    Scroll to the top when click on a new link

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import Router from "./routes/";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <Router />
       </BrowserRouter>
     </AuthProvider>
